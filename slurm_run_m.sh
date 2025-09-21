e0afd8b2278b989f92a966947e9826d69c98c28f
giang-n-ngo <giang.ngo.nam@gmail.com>
2025-09-21 12:27:26 +1000

    Update

--- slurm_run_m.sh
+++ slurm_run_m.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gpus=a100:1
-#SBATCH --time=18:00:00 
+#SBATCH --time=10:00:00 
 #SBATCH --mem=48GB                    
 #SBATCH --cpus-per-gpu=48             
 #SBATCH --qos=batch-short
@@ -21,28 +21,28 @@ CUDA_LAUNCH_BLOCKING=1
 
 problems=(
     # "Ackley" 
-    # "Beale" 
+    "Beale" 
     # "Bukin" 
     # "Cosine8" 
-    # "DixonPrice" 
+    "DixonPrice" 
     # "DropWave" 
     # "EggHolder" 
     # "Griewank" 
     # "Hartmann" 
     # "HolderTable" 
-    # "Levy" 
+    "Levy" 
     # "Michalewicz" 
     # "Shekel" 
-    # "SixHumpCamel" 
-    # "StyblinskiTang" 
-    "BucheRastrigin" 
-    "LinearSlope" 
-    "AttractiveSector" 
-    "StepEllipsoid" 
-    "Discus" 
+    "SixHumpCamel" 
+    "StyblinskiTang" 
+    # "BucheRastrigin" 
+    # "LinearSlope" 
+    # "AttractiveSector" 
+    # "StepEllipsoid" 
+    # "Discus" 
     "BentCigar" 
-    "SharpRidge" 
-    "DifferentPowers" 
+    # "SharpRidge" 
+    # "DifferentPowers" 
     # "Weierstrass" 
     # "SchaffersIllCond" 
     # "CompositeGriewankRosenbrock" 
@@ -84,7 +84,7 @@ run_batch() {
 
 # Split into batches of 12 problems for 20 problems
 echo "Running problems in batches of 12..."
-run_batch "${problems[@]:0:12}"    # Problems 0-11
+run_batch "${problems[@]:0:6}"    # Problems 0-11
 # run_batch "${problems[@]:12:12}"   # Problems 12-23
 # run_batch "${problems[@]:24:12}"   # Problems 24-35
 # run_batch "${problems[@]:30:3}"   # Problems 30-32
