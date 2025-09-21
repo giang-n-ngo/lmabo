771f1b59392fa3cd7995e260411d4cdbee16dbe8
giang-n-ngo <giang.ngo.nam@gmail.com>
2025-09-21 18:53:26 +1000

    Update

--- slurm_run_m.sh
+++ slurm_run_m.sh
@@ -28,47 +28,47 @@ problems=(
     # "DropWave" 
     # "EggHolder" 
     # "Griewank" 
-    # "Hartmann" 
-    # "HolderTable" 
+    "Hartmann" 
+    "HolderTable" 
     # "Levy" 
     # "Michalewicz" 
-    # "Shekel" 
-    # "SixHumpCamel" 
+    "Shekel" 
+    "SixHumpCamel" 
     # "StyblinskiTang" 
     # "BucheRastrigin" 
-    # "LinearSlope" 
+    "LinearSlope" 
     # "AttractiveSector" 
-    # "StepEllipsoid" 
+    "StepEllipsoid" 
     # "Discus" 
     # "BentCigar" 
     # "SharpRidge" 
     # "DifferentPowers" 
     # "Weierstrass" 
     # "SchaffersIllCond" 
-    "CompositeGriewankRosenbrock" 
-    "Gallagher21" 
+    # "CompositeGriewankRosenbrock" 
+    # "Gallagher21" 
     # "Gallagher101" 
-    "Katsuura" 
+    # "Katsuura" 
     # "LunacekBiRastrigin" 
     # "hpt_breast_RandomForest"
     # "hpt_breast_DecisionTree"
     # "hpt_breast_SVM"
     # "hpt_breast_AdaBoost"
     # "hpt_breast_MLPSGD"
-    "hpt_digits_RandomForest"
+    # "hpt_digits_RandomForest"
     # "hpt_digits_DecisionTree"
     # "hpt_digits_SVM"
-    "hpt_digits_AdaBoost"
-    "hpt_digits_MLPSGD"
-    "hpt_wine_RandomForest"
-    "hpt_wine_DecisionTree"
+    # "hpt_digits_AdaBoost"
+    # "hpt_digits_MLPSGD"
+    # "hpt_wine_RandomForest"
+    # "hpt_wine_DecisionTree"
     # "hpt_wine_SVM"
     # "hpt_wine_AdaBoost"
     # "hpt_wine_MLPSGD"
-    "hpt_diabetes_RandomForest"
+    # "hpt_diabetes_RandomForest"
     "hpt_diabetes_DecisionTree"
     # "hpt_diabetes_SVM"
-    "hpt_diabetes_AdaBoost"
+    # "hpt_diabetes_AdaBoost"
     "hpt_diabetes_MLPSGD"
 )
 
@@ -84,9 +84,9 @@ run_batch() {
 
 # Split into batches of 12 problems for 20 problems
 echo "Running problems in batches of 12..."
-run_batch "${problems[@]:0:6}"    # Problems 0-11
-run_batch "${problems[@]:6:6}"   # Problems 12-23
-# run_batch "${problems[@]:24:12}"   # Problems 24-35
+run_batch "${problems[@]:0:8}"    # Problems 0-7
+# run_batch "${problems[@]:8:6}"   # Problems 8-13
+# run_batch "${problems[@]:14:12}"   # Problems 14-25
 # run_batch "${problems[@]:30:3}"   # Problems 30-32
 # run_batch "${problems[@]:48:3}"   # Problems 48-50
 # run_batch "${problems[@]:51:3}"   # Problems 51-53
