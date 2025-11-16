1ce1753f97b2260ea1e9fb5db923ddf4008e8113
giantonia <giang.ngo.nam@gmail.com>
2025-11-17 02:27:06 +1100

    Update

--- slurm_run.sh
+++ slurm_run.sh
@@ -23,52 +23,52 @@ problems=(
     # # "Beale" 
     # "Bukin" 
     # # "Cosine8" 
-    # # "DixonPrice" 
+    # "DixonPrice" 
     # "DropWave" 
     # "EggHolder" 
     # "Griewank" 
-    # # "Hartmann" 
+    # "Hartmann" 
     # "HolderTable" 
     # # "Levy" 
     # # "Michalewicz" 
-    # # "Shekel" 
+    # "Shekel" 
     # # "SixHumpCamel" 
-    # # "StyblinskiTang" 
+    # "StyblinskiTang" 
     # "BucheRastrigin" 
     # # "LinearSlope" 
-    # # "AttractiveSector" 
-    # # "StepEllipsoid" 
-    # # "Discus" 
-    # # "BentCigar" 
+    # "AttractiveSector" 
+    # "StepEllipsoid" 
+    "Discus" 
+    "BentCigar" 
     # "SharpRidge" 
     # "DifferentPowers" 
     # "Weierstrass" 
     # "SchaffersIllCond" 
     # "CompositeGriewankRosenbrock" 
-    # # "Gallagher21" 
+    # "Gallagher21" 
     # "Gallagher101" 
-    "Katsuura" 
-    "LunacekBiRastrigin" 
-    # "hpt_breast_RandomForest"
-    "hpt_breast_DecisionTree"
-    "hpt_breast_SVM"
-    # "hpt_breast_AdaBoost"
-    "hpt_breast_MLPSGD"
-    "hpt_digits_RandomForest"
-    "hpt_digits_DecisionTree"
-    # "hpt_digits_SVM"
-    # "hpt_digits_AdaBoost"
-    "hpt_digits_MLPSGD"
-    # "hpt_wine_RandomForest"
-    "hpt_wine_DecisionTree"
-    # "hpt_wine_SVM"
-    # "hpt_wine_AdaBoost"
-    # "hpt_wine_MLPSGD"
-    # "hpt_diabetes_RandomForest"
-    "hpt_diabetes_DecisionTree"
+    # "Katsuura" 
+    # "LunacekBiRastrigin" 
+    # # "hpt_breast_RandomForest"
+    # "hpt_breast_DecisionTree"
+    # "hpt_breast_SVM"
+    # # "hpt_breast_AdaBoost"
+    # "hpt_breast_MLPSGD"
+    # "hpt_digits_RandomForest"
+    # "hpt_digits_DecisionTree"
+    # # "hpt_digits_SVM"
+    # # "hpt_digits_AdaBoost"
+    # "hpt_digits_MLPSGD"
+    # # "hpt_wine_RandomForest"
+    # "hpt_wine_DecisionTree"
+    # # "hpt_wine_SVM"
+    # # "hpt_wine_AdaBoost"
+    # # "hpt_wine_MLPSGD"
+    # # "hpt_diabetes_RandomForest"
+    # "hpt_diabetes_DecisionTree"
     # "hpt_diabetes_SVM"
-    # "hpt_diabetes_AdaBoost"
-    "hpt_diabetes_MLPSGD"
+    # # "hpt_diabetes_AdaBoost"
+    # "hpt_diabetes_MLPSGD"
 )
 
 # Function to run problems in batches
@@ -90,12 +90,12 @@ run_batch() {
     wait  # Wait for this batch to complete
 }
 
-# Split into batches of 6 problems for 12 problems
-echo "Running problems in batches of 6..."
-run_batch "${problems[@]:0:6}"    # Problems 0-5
-run_batch "${problems[@]:6:6}"   # Problems 6-11
-# run_batch "${problems[@]:12:6}"    # Problems 12-17
-# run_batch "${problems[@]:18:6}"    # Problems 18-23
+# Split into batches of 3 problems for 12 problems
+echo "Running problems in batches of 4..."
+run_batch "${problems[@]:0:4}"    # Problems 0-3
+# run_batch "${problems[@]:4:4}"   # Problems 4-7
+# run_batch "${problems[@]:8:4}"    # Problems 8-11
+# run_batch "${problems[@]:12:4}"    # Problems 12-15
 # run_batch "${problems[@]:16:4}"   # Problems 16-19
 # run_batch "${problems[@]:20:4}"   # Problems 20-23
 # run_batch "${problems[@]:24:4}"   # Problems 24-27
